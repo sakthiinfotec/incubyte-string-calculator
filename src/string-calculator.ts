ded796c4f5365373ef0f2926e7351717598c905a
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2025-03-02 16:38:14 +0530

    Fixed should return 6 when '1,5' passed - [Test Passed]

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -13,6 +13,9 @@ export class StringCalculator {
     if (numbers === "") {
       return 0;
     }
+    if (numbers.includes(",")) {
+      return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
+    }
     return parseInt(numbers);
   }
 }
