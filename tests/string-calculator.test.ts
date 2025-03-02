fc50b14adfc05639a683adb76f8d04acd3098243
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2025-03-02 18:43:20 +0530

    Added test should throw 'Negative numbers not allowed -2' - [Test Failed]

--- tests/string-calculator.test.ts
+++ tests/string-calculator.test.ts
@@ -4,31 +4,43 @@ describe("String Calculator", () => {
   it("should return 0 when passed an empty string", () => {
     const calculator = new StringCalculator();
     const result = calculator.add("");
-    expect(result).toBe(0);
+    const expected = 0;
+    expect(result).toBe(expected);
   });
   it("should return 1 when '1' passed", () => {
     const calculator = new StringCalculator();
     const result = calculator.add("1");
-    expect(result).toBe(1);
+    const expected = 1;
+    expect(result).toBe(expected);
   });
   it("should return 6 when '1,5' passed", () => {
     const calculator = new StringCalculator();
     const result = calculator.add("1,5");
-    expect(result).toBe(6);
+    const expected = 6;
+    expect(result).toBe(expected);
   });
   it("should return 55 when '1,2,3,4,5,6,7,8,9,10' passed", () => {
     const calculator = new StringCalculator();
     const result = calculator.add("1,2,3,4,5,6,7,8,9,10");
-    expect(result).toBe(55);
+    const expected = 55;
+    expect(result).toBe(expected);
   });
   it("should return 6 when '1\n2,3' passed (combination of \n and ,)", () => {
     const calculator = new StringCalculator();
     const result = calculator.add("1\n2,3");
-    expect(result).toBe(6);
+    const expected = 6;
+    expect(result).toBe(expected);
   });
   it("should return 3 when '//;\n1;2' passed + default delimiter is ';'", () => {
     const calculator = new StringCalculator();
     const result = calculator.add("//;\n1;2");
-    expect(result).toBe(3);
+    const expected = 3;
+    expect(result).toBe(expected);
+  });
+  it("should throw 'Negative numbers not allowed -2'", () => {
+    const calculator = new StringCalculator();
+    const numbers = "-2";
+    const expected = `Negative numbers not allowed ${numbers}`;
+    expect(() => calculator.add(numbers)).toThrow(expected);
   });
 });
